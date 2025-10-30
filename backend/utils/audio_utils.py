287efedb9783336bc768e77d39901389e80cf21d
Swaraag <swaraag.sistla@gmail.com>
2025-10-29 22:17:44 -0700

    finish audio_utils.py outline

--- backend/utils/audio_utils.py
+++ backend/utils/audio_utils.py
@@ -1,6 +1,21 @@
-
-
-#def resample_audio():
-
+import numpy as np
+import librosa
 
 def bytes_to_audio_array(pcm_bytes):
+    '''Takes the raw bytes, converts to a numpy array at 48khz'''
+    if len(pcm_bytes) == 0:
+        raise ValueError("Empty audio bytes received.")
+    np_audio = np.frombuffer(pcm_bytes, dtype=np.int16).astype(np.float32) / 32768.0
+    return np_audio
+
+
+def resample_audio(input_audio, orig_sr=48000, target_sr=16000):
+    '''Takes the numpy array at 48khz and resamples down to 16khz'''
+    resampled_audio = librosa.resample(y=input_audio, orig_sr=orig_sr, target_sr=target_sr)
+    return resampled_audio
+
+def process_audio_bytes(pcm_bytes):
+    '''Combines both functions into one step that will be used in main.py'''
+    audio_array = bytes_to_audio_array(pcm_bytes)
+    resampled = resample_audio(audio_array, orig_sr, target_sr)
+    return resampled
