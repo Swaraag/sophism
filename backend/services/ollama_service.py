2e1ea1f6f58f8d4865913d7ba1d8cd264ed6b708
Swaraag <swaraag.sistla@gmail.com>
2025-11-04 13:03:37 -0800

    draft detect_fallacies function in ollama_service.py

--- backend/services/ollama_service.py
+++ backend/services/ollama_service.py
@@ -9,3 +9,24 @@ async def init_ollama():
         print("The Ollama service is ready and working.")
     except ollama.ResponseError as e:
         raise Exception(f"Ollama error: {e}")
+    
+async def detect_fallacies(transcript):
+    # transcript format: transcript = [{"speaker": "speaker1", "start": 0:00, "end": 0:25, "transcript": "Hi my name is..."}, ...]
+
+    string_transcript = ""
+    for dialogue in transcript:
+        string_transcript += f"{dialogue["speaker"]} ({dialogue["start"]} - {dialogue["end"]}s): {dialogue[transcript]}"
+
+    messages = [
+        {'role': 'system', 'content': 'You are an expert on logical fallacies and debate. '
+        'Given this transcript, try to detect fallacies from either speaker. If no fallacy is detected, then respond with nothing. '
+        'If a fallacy is detected, respond with JSON that provides the speaker who committed the fallacy, the type of fallacy, '
+        'the statement which caused the fallacy, an explanation of why it is a fallacy, and a confidence level (between 0 and 1) for how likely it is to be a fallacy.'
+        'The names of the fields are "speaker", "fallacy_type", "statement", "explanation", "confidence".'
+        'Use the entire transcript for context, but only look for fallacies in the last couple dialogues or in relatively recent history.'},
+        {'role': 'user', 'content': string_transcript}
+        ]
+    
+    response = ollama.chat(model='llama3.1:8b', messages=messages)
+    
+    
