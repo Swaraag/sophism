4b796c6d551ba15cd9a7c965a6b80547f21dcfe2
Swaraag <swaraag.sistla@gmail.com>
2025-11-20 22:10:52 -0800

    streamline the process to deal with data being sent way too often

--- frontend/public/audio-processor-worklet.js
+++ frontend/public/audio-processor-worklet.js
@@ -1,10 +1,21 @@
 class AudioProcessorWorklet extends AudioWorkletProcessor {
     constructor() {
         super();
+        this.buffer = new Float32Array(16000);
+        this.bufferIndex = 0; // 1 second at 48kHz, 3 seconds at 16kHz
+        this.bufferSize = 16000;
+
     }
     process(inputs, outputs, parameters) {
-        if (inputs[0][0]) {
-            this.port.postMessage({ audioData: inputs[0][0]})
+        const inputChannel = inputs[0][0]
+        if (inputChannel) {
+            this.buffer.set(inputChannel, this.bufferIndex)
+            this.bufferIndex += inputChannel.length;
+
+            if (this.bufferIndex >= this.bufferSize) {
+                this.port.postMessage({ audioData: this.buffer.slice(0, this.bufferIndex)});
+                this.bufferIndex = 0;
+            }
         }
         return true;
     }
