dcfdbf141714a7d3580d8f3893e5468abb6d91c9
Swaraag <swaraag.sistla@gmail.com>
2025-12-29 14:16:06 -0800

    introduce toggle for showing more information about fallacy

--- frontend/src/components/FallacyDisplay.jsx
+++ frontend/src/components/FallacyDisplay.jsx
@@ -1,9 +1,55 @@
+import { useState } from 'react';
 export default function FallacyDisplay({ fallacies }) {
-    const fallacyText = fallacies.map((element, index) => <li key={index}><strong>{element["speaker"]}: {element["fallacy_type"]}</strong></li>)
+    let [expandedFallacies, updateExpandedFallacies] = useState(new Set());
+    const addFallacy = (fallacy) => {
+        updateExpandedFallacies(expandedFallacies => new Set(expandedFallacies).add(fallacy));
+    }
+    const removeFallacy = (fallacy) => {
+        updateExpandedFallacies(expandedFallacies => {
+            const newSet = new Set(expandedFallacies)
+            newSet.delete(fallacy);
+            return newSet
+        });
+    };
+
+    function fallacyClicked(event) {
+        console.log("Clicked!")
+        // .closest() ensures that even clicking a child of the list element ends up finding the right item
+        const clickedItem = event.target.closest('li');
+        if (clickedItem) {
+            const itemKey = Number(clickedItem.dataset.key);
+            if (expandedFallacies.has(itemKey)) {
+                console.log("Adding!")
+                removeFallacy(itemKey);
+            }
+            else {
+                console.log("Removing!")
+                addFallacy(itemKey);
+            }
+
+        }
+    }
+
     return (
         <>
         <h3>Fallacies</h3>
-        <ul>{fallacyText}</ul>
+        <ul onClick={fallacyClicked}>
+            {fallacies.map((element, index) => 
+                <li key={index} data-key={index} data-is-expanded={expandedFallacies.has(index)}>
+                    <div className="fallacy-header">
+                        <strong>{element["speaker"]}: {element["fallacy_type"]}</strong>
+                    </div>
+                    {expandedFallacies.has(index) && (
+                    <div className="fallacy-details">
+                        <strong>Statement: </strong>{element["statement"]}<br/>
+                        <strong>Explanation:</strong> {element["explanation"]}<br/>
+                        <strong>Confidence:</strong> {element["confidence"]}<br/>
+                    </div>
+                    )}
+                </li>
+                )
+            }
+        </ul>
         </>
     )
 }
--- frontend/src/components/TranscriptDisplay.jsx
+++ frontend/src/components/TranscriptDisplay.jsx
@@ -1,12 +1,18 @@
 
 export default function TranscriptDisplay({ transcript }) {
     // transcript format: [{"speaker": "speaker1", "start": 0:00, "end": 0:25, "transcript": "What's up?"}, ...]
-    const transcriptText = transcript.map((element, index) => <li key={index}><strong>{element["speaker"]}</strong>{": " + element["transcript"] + " (" + element["start"].toFixed(2) + "-" + element["end"].toFixed(2) + ")"}</li>)
 
     return (
         <>
         <h3>Transcript</h3>
-        <ul>{transcriptText}</ul>
+        <ul>
+            {transcript.map((element, index) => 
+                <li key={index}>
+                    <strong>{element["speaker"]}</strong>
+                    {": " + element["transcript"] + " (" + element["start"].toFixed(2) + "-" + element["end"].toFixed(2) + ")"}
+                </li>)
+            }
+        </ul>
         </>
     )
 }
